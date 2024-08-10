8b4079874d4be9f6e8f07f57b1cdfa7c06a851d6
yusuf <horiyorrmi72@gmail.com>
2024-08-10 23:26:14 +0100

    refactoring

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 4040;
+const routes = require('./routes');
 const app = express();
+const db = require('./config/db');
+const handleError = require('./controllers/errorController');
+const version = 'v1';
+const environment = process.env.NODE_ENV;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/api/v1', routes);
 
 app.get('/', async (req, res) => {
 	res.status(200).json({
@@ -15,13 +21,13 @@ app.get('/', async (req, res) => {
 		data: 'welcome to our custom tour server side',
 	});
 });
+app.use(handleError);
 
-const environment = process.env.NODE_ENV;
-
+db;
 app.listen(port, () => {
 	environment.match('development')
 		? console.log(
-				`listening 👂 and running 🏃 👣 on port: ${port} and you're in dev-mode 👨‍💻 💻`
+				`Server listening 👂 and running 🏃 👣 on port: ${port} and you're in dev-mode 👨‍💻 💻`
 		  )
-		: console.log(`listening 👂 and running 🏃 👣 on port: ${port}`);
+		: console.log(`Server listening 👂 and running 🏃 👣 on port: ${port}`);
 });
