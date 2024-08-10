7f641b9eebd374c6fd9aaa1fe8bf0e20c2e5f573
yusuf <horiyorrmi72@gmail.com>
2024-08-10 23:29:03 +0100

    clean up

--- utils/appError.js
+++ utils/appError.js
@@ -1,11 +1,9 @@
 class AppError extends Error {
-	constructor(status, message, statusCode) {
+	constructor(message, statusCode) {
 		super(message);
-
 		this.statusCode = statusCode;
 		this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-		this.operational = true;
-
+		this.isOperational = true;
 		Error.captureStackTrace(this, this.constructor);
 	}
 }
