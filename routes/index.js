9eab4f5bfdaeb9c1d3ba9aa9fb803f4953196e9e
yusuf <horiyorrmi72@gmail.com>
2024-08-10 23:41:00 +0100

    base route file

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const userRoute = require('./usersRoutes');
+const authRoute = require('./authRoute');
+const tourRoute = require('./tourRoutes');
+const bookingRoute = require('./bookingRoutes');
+
+
+const router = express();
+
+
+
+router.use('/auth',authRoute);
+router.use('/user',userRoute);
+router.use('/tour',tourRoute);
+router.use('/booking',bookingRoute);
+
+
+module.exports = router;
+
