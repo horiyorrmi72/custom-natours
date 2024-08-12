c56c08a21033954a46a28ba3495517be6d805753
yusuf <horiyorrmi72@gmail.com>
2024-08-12 16:30:02 +0100

    cleanup

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const authController = require('../controllers/authController');
-const handleError = require('../controllers/errorController');
 const router = express();
-router.use(handleError);
 
 router.post('/signup', authController.signup);
 router.post('/login' ,authController.signin);
-router.post('/forgotPassword');
+router.post('/forgotPassword', authController.forgotPassword);
 router.post('/resetPassword');
 
 module.exports = router;
