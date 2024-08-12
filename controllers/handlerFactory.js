cdf1049d54b5eb79d06b020578801a61c83badab
yusuf <horiyorrmi72@gmail.com>
2024-08-12 16:02:32 +0100

    update utils file name in this file

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,5 +1,5 @@
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync.utils');
+const AppError = require('../utils/appError.utils');
 const { Model } = require('mongoose');
 
 /**
